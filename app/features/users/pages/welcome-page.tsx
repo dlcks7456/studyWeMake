5ab8b04199d449d3d4e06ced9ab0590fadec8624
Chan lee <dlcks17@kakao.com>
2025-09-07 13:03:04 +0900

    welcome-page delete

--- app/features/users/pages/welcome-page.tsx
+++ app/features/users/pages/welcome-page.tsx
@@ -1,16 +1,16 @@
-import { Resend } from "resend";
-import { render } from "@react-email/components";
-import { WelcomeUserEmail } from "react-email-starter/emails/welcome-user";
-import type { Route } from "./+types/welcome-page";
+// import { Resend } from "resend";
+// import { render } from "@react-email/components";
+// import { WelcomeUserEmail } from "react-email-starter/emails/welcome-user";
+// import type { Route } from "./+types/welcome-page";
 
-const client = new Resend(process.env.RESEND_API_KEY);
+// const client = new Resend(process.env.RESEND_API_KEY);
 
-export const loader = async ({ params }: Route.LoaderArgs) => {
-	const { data, error } = await client.emails.send({
-		from: "Chuck <chuck@mail.chachachanchacha.xyz>",
-		to: ["dlcks17@naver.com"],
-		subject: "Welcome to Chachachanchacha",
-		react: <WelcomeUserEmail username={params.username} />,
-	});
-	return Response.json({ data, error });
-};
+// export const loader = async ({ params }: Route.LoaderArgs) => {
+// 	const { data, error } = await client.emails.send({
+// 		from: "Chuck <chuck@mail.chachachanchacha.xyz>",
+// 		to: ["dlcks17@naver.com"],
+// 		subject: "Welcome to Chachachanchacha",
+// 		react: <WelcomeUserEmail username={params.username} />,
+// 	});
+// 	return Response.json({ data, error });
+// };
